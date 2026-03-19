7f85ee7840a98cf4e7564e2ec408d01512b3cfc5
nikolas923 <nikolamor3@gmail.com>
2026-03-19 15:43:45 +0300

    Refactor conftest

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,10 +1,3 @@
-import os
-import time
-from selenium import webdriver
-
-BASE_DIR=os.path.dirname(os.path.dirname(__file__))
-DRIVER_PATH=os.path.join(BASE_DIR, 'chromedriver.exe')
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('http//demoqa.com')
-time.sleep(2)
-driver.quit()
+def test_main_page_is_evalable(driver):
+    driver.get('https://demoqa.com/')
+    assert driver.title=='demosite'
